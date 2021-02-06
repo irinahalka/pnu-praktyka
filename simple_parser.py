83663ccd12ac6c492be4d7bebe45e1aacd2d2d49
irinahalka <opiumandpoison.n@gmail.com>
2021-02-06 14:10:48 +0200

    simple parser

---
+++ simple_parser.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("https://en.numista.com/catalogue/index.php?ct=coin")
+soup = BeautifulSoup(html, "html.parser")
+
+all_items = soup.findAll("ul", class_="liste_pays")
+for item in all_items :
+    countries = item.findAll("li")
+    for country in countries:
+        country_item = country.find("a")
+        print(country_item.get_text())
+        print(country_item.get('href'))
+
+
+
+
